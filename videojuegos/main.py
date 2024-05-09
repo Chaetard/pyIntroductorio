cd0adaf3b736c673eac2339209e928aa677bc657
Chaetard <jesus.santos4160@alumnos.udg.mx>
2024-05-09 15:36:24 -0600

    actualizacion de proyecto de vieojuegos

--- videojuegos/main.py
+++ videojuegos/main.py
@@ -4,10 +4,6 @@ import controlador_juegos
 
 
 app = Flask(__name__)
-app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='juegos'
 
 @app.route("/agregar_juego")
 def formulario_agregar_juego():
